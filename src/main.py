e6bd6b620a79857ba5439278d66bb3002e524f06
Nikita Silaev <Beeeirym@gmail.com>
2025-09-26 20:55:23 +0300

    chore: Initializing a Telegram bot

---
+++ src/main.py
@@ -0,0 +1,23 @@
+from aiogram import Bot, Dispatcher
+
+from config import settings
+from handlers import common
+
+import asyncio
+
+
+async def main() -> None:
+    bot = Bot(token=settings.TELEGRAM_BOT_TOKEN)
+    dp = Dispatcher()
+
+    # Handlers routers
+    dp.include_router(common.router)
+
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, RuntimeError):
+        print('Bot has been stopped.')
